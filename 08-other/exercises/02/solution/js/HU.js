6cb694cc841f936a83a718066843a0c7c9debbc2
GergelyPorvai <porvai.gergely@gmail.com>
2022-09-02 13:24:20 +0200

    v2

--- 08-other/exercises/02/solution/js/HU.js
+++ 08-other/exercises/02/solution/js/HU.js
@@ -1,1 +1,28 @@
+/* 2. Készíts egy `HU` nevű objektumot, aminek az alábbi propertijei vannak:
+- `date()`: Visszaadja a paraméterként megadott dátumot a magyar dátumírás
+szabályainak megfelelően
+- `currency()`: Visszaadja a paraméterként megadott számot a
+magyar pénzformátumnak megfelelően a Ft végződéssel együtt
+- `list()`: A paraméterként kapott string tömböt visszaadja az alábbi
+formában: első, második és harmadik"
+(Tehát vesszőkkel elválasztva az utolsó elem előtt az és szóval) */
+
+const HU = {
+  date(date) {
+    return new Intl.DateTimeFormat("hu-HU").format(date);
+  },
+  currency(amount) {
+    const hunCurrency = new Intl.NumberFormat("hu-HU", {
+      style: "currency",
+      currency: "HUF",
+    });
+    return hunCurrency.format(amount);
+  },
+  list(stringArray) {
+    const last = stringArray.pop();
+    const beforeLast = stringArray.pop();
+    return `${stringArray.map((item) => `${item}, `)}${beforeLast} és ${last}`;
+  },
+};
+
 export default HU;
--- 08-other/exercises/04/solution/js/customDateFormats.js
+++ 08-other/exercises/04/solution/js/customDateFormats.js
@@ -1,1 +1,55 @@
+/* 4. Írj egy függvényt `customDateFormats` néven, ami paraméterként egy Date objektumot kap,
+a visszatérési értéke pedig egy objektum két tulajdonsággal,
+ami a következő formátumban tartalmazza a paraméterként kapott dátumot:
+- `short`: 2020. jan. 11. 14:20 (tehát évszám, a hónap neve röviden magyarul,
+    kisbetűkkel a nap száma, majd az idő a másodperc nélkül)
+- `long`: 2020. január 11. 14:20:10 (tehát évszám, a hónap neve magyarul,
+    a nap száma, majd az idő) */
+
+const customDateFormats = (Date = new Date()) => {
+  const shortMonth = [
+    "jan.",
+    "febr.",
+    "márc.",
+    "ápr.",
+    "máj.",
+    "jún.",
+    "júl.",
+    "aug.",
+    "szept.",
+    "okt.",
+    "nov.",
+    "dec.",
+  ];
+  const longMonth = [
+    "január",
+    "február",
+    "március",
+    "április",
+    "május",
+    "június",
+    "július",
+    "augusztus",
+    "szeptember",
+    "október",
+    "november",
+    "december",
+  ];
+
+  const shortDate =
+    `${Date.getFullYear()}. ${
+      shortMonth[Date.getMonth()]
+    } ${Date.getDate()}. ` +
+    `${Date.getHours() < 10 ? `0${Date.getHours()}` : Date.getHours()}:` +
+    `${Date.getMinutes() < 10 ? `0${Date.getMinutes()}` : Date.getMinutes()}`;
+  const longDate =
+    `${Date.getFullYear()}. ${longMonth[Date.getMonth()]} ${Date.getDate()}. ` +
+    `${Date.toLocaleTimeString("hu")}`;
+
+  return {
+    short: shortDate,
+    long: longDate,
+  };
+};
+
 export default customDateFormats;
